99735b96e0b9e719838b8bef0f8de31b491c7529
Ahmed Ahmed <ahmed.ahmed.jobb@gmail.com>
2025-06-01 18:25:08 +0200

    la till cw för testa

--- WebApI-1/Program.cs
+++ WebApI-1/Program.cs
@@ -6,6 +6,7 @@ namespace WebApI_1
         public static void Main(string[] args)
         {
             Console.WriteLine("hey");
+            Console.WriteLine("hejdå");
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
